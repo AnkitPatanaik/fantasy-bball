77d5f3a8e9facc7972bb7c21fd1b82dc21cca64b
AnkitPatanaik <apatanai@purdue.edu>
2018-11-16 21:33:36 -0800

    can search for people

--- compare.py
+++ compare.py
@@ -1,1 +1,18 @@
 from basketball_reference_web_scraper import client
+
+YEAR_END = 2018
+
+def get_values(player_list):
+    values = []
+
+    players = client.players_season_totals(YEAR_END)
+    for player in players:
+        if player['name'] in player_list:
+            values.append(player)
+    return values 
+
+def main():
+    team_1 = raw_input("enter comma separated list with players from one team")
+    team_2 = raw_input("enter comma separated list with players from the other team")
+    
+print(get_values(["Joe Harris"]))
