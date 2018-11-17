53e2054beadc51918c3d3b9b84b4341ec5a5decb
AnkitPatanaik <apatanai@purdue.edu>
2018-11-17 11:09:33 -0800

    percentages

--- compare.py
+++ compare.py
@@ -1,11 +1,14 @@
 from basketball_reference_web_scraper import client
 
 YEAR_END = 2018
+PRECISION = 2
 
 def get_values(player_list_string):
     values = []
+    
     player_list = player_list_string.split(", ")
     player_list = [x.lower() for x in player_list] #iterates over list and makes everything lower()
+
     players = client.players_season_totals(YEAR_END)
     for player in players:
         if player['name'].lower() in player_list:
@@ -13,7 +16,8 @@ def get_values(player_list_string):
     return values 
 
 def get_stats(player_dict):
-    stats = {'points': 0, 'assists': 0, 'rebounds': 0, 'steals': 0, 'blocks': 0, 'made_field_goals': 0, 'attempted_field_goals': 0, 'made_free_throws': 0, 'attempted_free_throws': 0, '3pm': 0, 'turnovers' : 0}
+    stats = {'points': 0, 'assists': 0, 'rebounds': 0, 'steals': 0, 'blocks': 0, 'made_field_goals': 0, 'attempted_field_goals': 0,'fg%': 0,  'made_free_throws': 0, 'attempted_free_throws': 0, 'ft%': 0, '3pm': 0, 'turnovers' : 0}
+    
     for player in player_dict:
         stats['points'] += player['made_field_goals'] * 2 + player['made_three_point_field_goals'] + player['made_free_throws']
         stats['assists'] += player['assists']
@@ -26,6 +30,8 @@ def get_stats(player_dict):
         stats['attempted_free_throws'] += player['attempted_free_throws']
         stats['3pm'] += player['made_three_point_field_goals']
         stats['turnovers'] += player['turnovers']
+    stats['fg%'] = round(stats['made_field_goals']/stats['attempted_field_goals'] * 100, PRECISION)
+    stats['ft%'] = round(stats['made_free_throws']/stats['attempted_free_throws'] * 100, PRECISION)
     return stats
 
 
@@ -33,6 +39,5 @@ def main():
     team_1 = input("enter comma separated list with players from one team: \n")
     team1_players = get_values(team_1)
     print("STATS: \n", get_stats(team1_players))
-    #team_2 = raw_input("enter comma separated list with players from the other team")
     
 main()
